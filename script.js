ca092c5f8361ee64d64c3e0047191c223ffb399c
Enara <enara.artetxe3@gmail.com>
2023-04-11 20:38:42 +0200

    added canvas button for pinch/pan on iPad

--- script.js
+++ script.js
@@ -107,9 +107,9 @@ function init() {
     bg_canvas.style.right = "2vw";
     document.getElementById("container-v").style.left = "0px";
     alertWindow.style.left = "90vw";
-    canvasButton.style.left = "90vw";
+    canvasButton.style.left = "92vw";
 
-
+    
     canvas.addEventListener("touchstart", (e) => {
       touches = e.touches.length;
       if (touches > 1)
